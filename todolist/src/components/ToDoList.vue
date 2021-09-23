a343427203fd45e1b1ba81477c21454649912512
jimmy <y98765487@gmail.com>
2021-09-24 02:50:44 +0800

    add some js effects

--- todolist/src/components/ToDoList.vue
+++ todolist/src/components/ToDoList.vue
@@ -16,52 +16,55 @@
               focus:outline-none
               flex-1
               block
-              rounded-none rounded-r-md
+              rounded-none
               sm:text-sm
               border-gray-300
+              enter-input
             "
             placeholder="enter to add todo"
           />
         </span>
-        <div class="flex items-start" :key="todo.name" v-for="todo in todoList">
-          <div class="flex items-center h-5">
-            <label class="font-medium text-gray-700 mr-1">
-              <input
-                name="todo"
-                type="checkbox"
-                v-model="todo.completed"
+        <div class="flex items-start" :key="todo.id" v-for="todo in todoList">
+          <transition name="fade">
+            <div class="flex items-center h-5">
+              <label class="font-medium text-gray-700 mr-1">
+                <input
+                  name="todo"
+                  type="checkbox"
+                  v-model="todo.completed"
+                  class="
+                    checked:bg-blue-600
+                    checked:border-transparent
+                    focus:ring-indigo-500
+                    h-4
+                    w-4
+                    text-indigo-600
+                    border-gray-300
+                    rounded
+                  "
+                />
+                {{ todo.name }}</label
+              >
+              <button
+                v-on:click="del(todo)"
                 class="
-                  checked:bg-blue-600
-                  checked:border-transparent
-                  focus:ring-indigo-500
-                  h-4
-                  w-4
-                  text-indigo-600
-                  border-gray-300
-                  rounded
+                  text-white text-sm
+                  inline-block
+                  py-1
+                  px-2
+                  bg-red-500
+                  text-white
+                  font-semibold
+                  rounded-lg
+                  shadow-md
+                  hover:bg-red-700
+                  focus:outline-none
                 "
-              />
-              {{ todo.name }}</label
-            >
-            <button
-              v-on:click="del(todo)"
-              class="
-                text-white text-sm
-                inline-block
-                py-1
-                px-2
-                bg-red-500
-                text-white
-                font-semibold
-                rounded-lg
-                shadow-md
-                hover:bg-red-700
-                focus:outline-none
-              "
-            >
-              del
-            </button>
-          </div>
+              >
+                <i class="fas fa-trash"></i>
+              </button>
+            </div>
+          </transition>
         </div>
       </div>
     </div>
@@ -70,7 +73,7 @@
         <h3 class="text-lg leading-6 font-medium text-gray-900">Completed：</h3>
         <span
           class="text-md transition-opacity"
-          :key="todo.name"
+          :key="todo.id"
           v-for="todo in todoList"
         >
           <transition name="fade">
@@ -92,19 +95,23 @@ export default {
       todoList: [],
     };
   },
-  props: {
-    msg: String,
-  },
   methods: {
     check(value) {
       this.todoList.forEach((todo) => {
         todo.completed = value;
       });
     },
+    randomKey() {
+      return Math.floor(Math.random() * 1000000) + 1;
+    },
     add(task) {
       if (!task) return;
 
-      this.todoList.push({ name: task, completed: false });
+      this.todoList.push({
+        id: this.randomKey(),
+        name: task,
+        completed: false,
+      });
       this.task = "";
     },
     del(todo) {
@@ -118,10 +125,15 @@ export default {
 <style scoped>
 .fade-enter-active,
 .fade-leave-active {
-  transition: opacity 0.5s;
+  transition: opacity ease 0.25s, transform ease-in-out 0.3s;
 }
-.fade-enter,
+.fade-enter-from,
 .fade-leave-to {
   opacity: 0;
+  transform: translateX(100%);
+}
+
+.enter-input:focus {
+  border-bottom: 1px solid #ddd;
 }
 </style>
